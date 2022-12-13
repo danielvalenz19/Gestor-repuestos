c9018930c695089f600ba6ab6712dcf7cf3378f5
Daniel Valenzuela <hetzondaniel1@gmail.com>
2022-12-13 06:40:16 -0600

    Se agrego la base de datos en el repositorio

---
+++ BD/transporte_mantenimiento_repuesto.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: transporte
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `mantenimiento_repuesto`
+--
+
+DROP TABLE IF EXISTS `mantenimiento_repuesto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mantenimiento_repuesto` (
+  `repuesto_id` int NOT NULL,
+  `mantenimiento_id` int NOT NULL,
+  `cantidad` int NOT NULL,
+  PRIMARY KEY (`repuesto_id`,`mantenimiento_id`),
+  KEY `fk_repuestos_has_mantenimientos_mantenimientos1_idx` (`mantenimiento_id`),
+  KEY `fk_repuestos_has_mantenimientos_repuestos1_idx` (`repuesto_id`),
+  CONSTRAINT `fk_repuestos_has_mantenimientos_mantenimientos1` FOREIGN KEY (`mantenimiento_id`) REFERENCES `mantenimientos` (`id`),
+  CONSTRAINT `fk_repuestos_has_mantenimientos_repuestos1` FOREIGN KEY (`repuesto_id`) REFERENCES `repuestos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mantenimiento_repuesto`
+--
+
+LOCK TABLES `mantenimiento_repuesto` WRITE;
+/*!40000 ALTER TABLE `mantenimiento_repuesto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mantenimiento_repuesto` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-13  6:39:38
